9b5f25558e3f033c05d29c955c31b611ac6a9605
OkabeRintarouBeta <88678246+OkabeRintarouBeta@users.noreply.github.com>
2022-04-14 19:00:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Stack from '@mui/material/Stack';
+
+import InputList from "./components/InputList";
+import TodoList from "./components/TodoList";
+
+
+import {useState} from "react";
+
+const drawerWidth = 300;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+
+
+function App() {
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+    const [taskList,setTaskList]=useState([])
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+
+    /*
+    Adapted from SI579 Todo List
+    @ see https://github.com/UMSI579/todo/blob/step19/src/TodoList.js
+    */
+
+    const displayTaskList = (items) => {
+        // console.log(items)
+        return items.map((item) =>
+            <TodoList
+                name={item.name}
+                description={item.description}
+                category={item.category}
+                timetodo={item.time}
+                remove={() => removeTask(item.name)}
+            />)
+    }
+
+    const removeTask=(itemName)=>{
+        setTaskList((previousList)=>{
+            const withItemRemoved = previousList.filter((item) => {
+                return item.name !== itemName
+            });
+            localStorage.setItem('my-todo-items', JSON.stringify(withItemRemoved));
+            return withItemRemoved;
+        })
+    }
+
+  return (
+    <div className="App">
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <AddCircleOutlineIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Mini variant drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <InputList isOpen={open} setTodoItems={setTaskList}/>
+            </Drawer>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+                <Toolbar />
+
+                <Box sx={{ width: '50%'}}>
+                    <Stack spacing={2}>
+                        {displayTaskList(taskList)}
+                    </Stack>
+                </Box>
+            </Box>
+        </Box>
+    </div>
+  );
+}
+
+export default App;
