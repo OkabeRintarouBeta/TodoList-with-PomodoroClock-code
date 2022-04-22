d10d55140f0af6f151422564ae2c8ff048badf1b
OkabeRintarouBeta <88678246+OkabeRintarouBeta@users.noreply.github.com>
2022-04-22 10:54:37 -0400

    Add files via upload

--- src/components_timer/CountdownTimerAnimation.js
+++ src/components_timer/CountdownTimerAnimation.js
@@ -9,7 +9,7 @@ import { SetupPomodoroContext } from "../context/SetupPomodoroContext";
     children: text content in the center
 */}
 
-const CountdownTimerAnimation = ({ keys, timerDuration, startAnimate, isDoingTask, children}) => {
+const CountdownTimerAnimation = ({ keys, timerDuration, startAnimate, children}) => {
     const { resetTimer } = useContext(SetupPomodoroContext) //import the stopTimer in SettingContext
     // console.log("keys: " + keys)
     // console.log("timerDuration" + timerDuration)
@@ -18,12 +18,14 @@ const CountdownTimerAnimation = ({ keys, timerDuration, startAnimate, isDoingTas
             key={keys}
             isPlaying={startAnimate}
             duration={ timerDuration * 60 } // sec * 60 => minutes
-            colors={"#A30000"}
-            strokeWidth={15}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[0.7*timerDuration * 60, 0.5*timerDuration * 60, 0.2*timerDuration * 60, 0]}
+            strokeWidth={20}
             size={220}
             trailColor="#D3D3D3" //the gray color after the color is disappearing
             onComplete={ () => {
-                resetTimer(isDoingTask);
+                // finishCycleHandler();
+                resetTimer();
             }}
         >   
             {children}
